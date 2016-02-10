f170d2b1d00927d0b541fc1d267a6118e3bde520
Andrew Hulbert <andrew.hulbert@ccri.com>
2016-02-10 16:33:39 -0500

    output files

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-curl http://www.trieuvan.com/apache/accumulo/1.6.4/accumulo-1.6.4-bin.tar.gz
-curl http://apache.claz.org/hadoop/common/hadoop-2.6.3/hadoop-2.6.3.tar.gz
+curl -o accumulo.tar.gz http://www.trieuvan.com/apache/accumulo/1.6.4/accumulo-1.6.4-bin.tar.gz
+curl -o hadoop.tar.gz  http://apache.claz.org/hadoop/common/hadoop-2.6.3/hadoop-2.6.3.tar.gz
 
